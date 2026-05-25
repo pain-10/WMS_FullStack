932a75a9813359978f2c8a70b42089b79ba12209
Mrunal <mrunal07shahare@gmail.com>
2026-05-25 20:54:17 +0530

    frontend issue

--- wms-frontend/src/app/config/api.config.ts
+++ wms-frontend/src/app/config/api.config.ts
@@ -1,2 +1,1 @@
-export const API_BASE_URL = 'https://wms-api-mrunal-e8awhsawexaeh2gx.eastasia-01.azurewebsites.net/api';
-//https://wms-api-mrunal-e8awhsawexaeh2gx.eastasia-01.azurewebsites.net/api
+export const API_BASE_URL = 'https://wms-api-mrunal-e8awhsawexaeh2gx.eastasia-01.azurewebsites.net';
